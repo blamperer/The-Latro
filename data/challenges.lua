95aca97933db8afdbc76762803aec59b8753af37
blamperer <137224671+blamperer@users.noreply.github.com>
2025-12-10 17:42:42 -0500

    update autopilot challenge
    - the ice cream is necessary
    - add restrictions (vanilla only for now)

--- data/challenges.lua
+++ data/challenges.lua
@@ -19,12 +19,40 @@ SMODS.Challenge({
 		{ id = "j_thel_helper_robot", edition = "negative", eternal = true },
 		{ id = "j_thel_helper_robot", edition = "negative", eternal = true },
 		{ id = "j_thel_helper_robot", edition = "negative", eternal = true },
+		{ id = "j_ice_cream" },
 	},
 	deck = { type = "Challenge Deck" },
 	rules = {
 		modifiers = {
-			{ id = "hands", value = 0 },
+			{ id = "hands",    value = 0 },
 			{ id = "discards", value = 0 },
 		},
 	},
+	restrictions = {
+		banned_cards = {
+			{ id = "j_banner" },
+			{ id = "j_delayed_grat" },
+			{ id = "j_burglar" },
+			{ id = "j_faceless" },
+			{ id = "j_luchador" },
+			{ id = "j_mail" },
+			{ id = "j_drunkard" },
+			{ id = "j_trading" },
+			{ id = "j_castle" },
+			{ id = "j_troubadour" },
+			{ id = "j_merry_andy" },
+			{ id = "j_hit_the_road" },
+			{ id = "j_burnt" },
+			{ id = "j_yorick" },
+			{ id = "v_grabber" },
+			{ id = "v_nacho_tong" },
+			{ id = "v_wasteful" },
+			{ id = "v_recyclomancy" },
+			{ id = "v_hieroglyph" },
+			{ id = "v_petroglyph" }
+		},
+		banned_tags = {
+			{ id = "tag_garbage" }
+		}
+	}
 })
